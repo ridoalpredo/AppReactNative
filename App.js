93ddca55ac24195e67a6bfcbe1c9b6a1975910f1
ridoalpredo <ridohaloho@gmail.com>
2020-12-29 01:31:05 -0800

    menambahkan komponent IMAGE

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { Image, Text, View } from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
 
-  render() {
-    return (
-      <View>
-        <Text> Hello World </Text>
-        <Text style={{color: "blue", fontSize: 14}}> Rido Alpredo </Text>
-      </View>
-
-    );
-  }
+const App = () => {
+  return (
+    <View>
+    <View style={{width: 80, height: 80, backgroundColor: "red", borderRadius: 50}}/>
+    <Text>Rido Alpredo</Text>
+    <Home />
+    <Gambar />
+    </View>
+  )
 }
+
+const Home = () => {
+  return <Text>Sihaloho</Text>
+};
+
+const Gambar = () => {
+  return (
+    <Image source={{uri: "https://placeimg.com/640/480/tech"}}
+    style={{width: 100 , height: 100, borderRadius: 10}}
+    />
+  )
+}
+
+export default App;
