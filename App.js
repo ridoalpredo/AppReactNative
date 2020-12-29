5da057268b293dc8549c29e105e7e9dde62bc663
ridoalpredo <ridohaloho@gmail.com>
2020-12-29 00:22:52 -0800

    merubah informasi default welcome react native

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
   render() {
     return (
       <View>
-        <Text> App </Text>
+        <Text> Hello World </Text>
       </View>
     );
   }
