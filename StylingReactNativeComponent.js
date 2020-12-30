756da26906691b595fbedb7431a5c63a77c51eb0
ridoalpredo <ridohaloho@gmail.com>
2020-12-30 11:16:38 -0800

    melakukan perubahan pada pada StylingReactNativeComponent

--- StylingReactNativeComponent.js
+++ StylingReactNativeComponent.js
@@ -6,224 +6,56 @@ import macbookair from './macbookair.jpg';
 const StylingReactNativeComponent = () => {
   return (
     <ScrollView>
-      <View style={{flexDirection: 'column', justifyContent: 'space-around'}}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-          <View>
-            <View style={{marginTop: -20}}>
+      <View style={{flexDirection: 'row'}}>
+        <View>
+          <View style={{marginTop: -20}}>
+            <View
+              style={{
+                backgroundColor: '#F2F2F2F2',
+                padding: 12,
+                width: 212,
+                borderRadius: 8,
+              }}>
+              <Image
+                source={macbookpro}
+                style={{width: 188, height: 107, borderRadius: 8}}
+              />
+
+              <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>
+                New Macbook Pro 2020
+              </Text>
+
+              <Text
+                style={{
+                  fontSize: 12,
+                  fontWeight: 'bold',
+                  marginTop: 14,
+                  color: 'orange',
+                }}>
+                Rp. 21.500.000
+              </Text>
+
+              <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
+                Jakarta Barat
+              </Text>
+
               <View
                 style={{
-                  backgroundColor: '#F2F2F2F2',
-                  padding: 12,
-                  width: 212,
-                  borderRadius: 8,
+                  borderWidth: 1,
+                  backgroundColor: '#6FCf92',
+                  borderRadius: 25,
+                  paddingVertical: 6,
+                  marginTop: 20,
                 }}>
-                <Image
-                  source={macbookpro}
-                  style={{width: 188, height: 107, borderRadius: 8}}
-                />
-
-                <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>
-                  New Macbook Pro 2020
-                </Text>
-
                 <Text
                   style={{
-                    fontSize: 12,
-                    fontWeight: 'bold',
-                    marginTop: 14,
-                    color: 'orange',
+                    fontWeight: '600',
+                    fontSize: 18,
+                    textAlign: 'center',
+                    color: 'white',
                   }}>
-                  Rp. 21.500.000
+                  BELI
                 </Text>
-
-                <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
-                  Jakarta Barat
-                </Text>
-
-                <View
-                  style={{
-                    borderWidth: 1,
-                    backgroundColor: '#6FCf92',
-                    borderRadius: 25,
-                    paddingVertical: 6,
-                    marginTop: 20,
-                  }}>
-                  <Text
-                    style={{
-                      fontWeight: '600',
-                      fontSize: 18,
-                      textAlign: 'center',
-                      color: 'white',
-                    }}>
-                    BELI
-                  </Text>
-                </View>
-              </View>
-            </View>
-          </View>
-          <View>
-            <View style={{marginTop: -20}}>
-              <View
-                style={{
-                  backgroundColor: '#F2F2F2F2',
-                  padding: 12,
-                  width: 212,
-                  borderRadius: 8,
-                }}>
-                <Image
-                  source={macbookair}
-                  style={{width: 188, height: 107, borderRadius: 8}}
-                />
-
-                <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>
-                  New Macbook Air M1 2020
-                </Text>
-
-                <Text
-                  style={{
-                    fontSize: 12,
-                    fontWeight: 'bold',
-                    marginTop: 14,
-                    color: 'orange',
-                  }}>
-                  Rp. 21.300.000
-                </Text>
-
-                <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
-                  Jakarta Timur
-                </Text>
-
-                <View
-                  style={{
-                    borderWidth: 1,
-                    backgroundColor: '#6FCf92',
-                    borderRadius: 25,
-                    paddingVertical: 6,
-                    marginTop: 20,
-                  }}>
-                  <Text
-                    style={{
-                      fontWeight: '600',
-                      fontSize: 18,
-                      textAlign: 'center',
-                      color: 'white',
-                    }}>
-                    BELI
-                  </Text>
-                </View>
-              </View>
-            </View>
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            marginTop: 15,
-          }}>
-          <View>
-            <View style={{marginTop: -20}}>
-              <View
-                style={{
-                  backgroundColor: '#F2F2F2F2',
-                  padding: 12,
-                  width: 212,
-                  borderRadius: 8,
-                }}>
-                <Image
-                  source={macbookpro}
-                  style={{width: 188, height: 107, borderRadius: 8}}
-                />
-
-                <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>
-                  New Macbook Pro 2020
-                </Text>
-
-                <Text
-                  style={{
-                    fontSize: 12,
-                    fontWeight: 'bold',
-                    marginTop: 14,
-                    color: 'orange',
-                  }}>
-                  Rp. 21.500.000
-                </Text>
-
-                <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
-                  Jakarta Barat
-                </Text>
-
-                <View
-                  style={{
-                    borderWidth: 1,
-                    backgroundColor: '#6FCf92',
-                    borderRadius: 25,
-                    paddingVertical: 6,
-                    marginTop: 20,
-                  }}>
-                  <Text
-                    style={{
-                      fontWeight: '600',
-                      fontSize: 18,
-                      textAlign: 'center',
-                      color: 'white',
-                    }}>
-                    BELI
-                  </Text>
-                </View>
-              </View>
-            </View>
-          </View>
-          <View>
-            <View style={{marginTop: -20}}>
-              <View
-                style={{
-                  backgroundColor: '#F2F2F2F2',
-                  padding: 12,
-                  width: 212,
-                  borderRadius: 8,
-                }}>
-                <Image
-                  source={macbookair}
-                  style={{width: 188, height: 107, borderRadius: 8}}
-                />
-
-                <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>
-                  New Macbook Pro M1 2018
-                </Text>
-
-                <Text
-                  style={{
-                    fontSize: 12,
-                    fontWeight: 'bold',
-                    marginTop: 14,
-                    color: 'orange',
-                  }}>
-                  Rp. 32.300.000
-                </Text>
-
-                <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
-                  Jakarta Pusat
-                </Text>
-
-                <View
-                  style={{
-                    borderWidth: 1,
-                    backgroundColor: '#6FCf92',
-                    borderRadius: 25,
-                    paddingVertical: 6,
-                    marginTop: 20,
-                  }}>
-                  <Text
-                    style={{
-                      fontWeight: '600',
-                      fontSize: 18,
-                      textAlign: 'center',
-                      color: 'white',
-                    }}>
-                    BELI
-                  </Text>
-                </View>
               </View>
             </View>
           </View>
